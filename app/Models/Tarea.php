80880f2447f8307ecc8cf18ebd293acd4fcd93b1
ginoliza <ginoangelolr@gmail.com>
2024-10-27 12:54:14 -0500

    C, D done

--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tarea extends Model {
     use HasFactory;
 
-    protected $fillable = ['descripcion', 'user_id', 'completed'];
+    protected $fillable = ['descripcion', 'user_id', 'completed', 'categoria_id'];
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function categoria() {
+        return $this->belongsTo(Categoria::class);
     }
 }
